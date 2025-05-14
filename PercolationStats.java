681bd00c1cb6ab8a879efafa684fa60029fcc30b
BlackCat1010 <shengjunho@gmail.com>
2025-05-15 00:33:35 +0800

    Commit on Main as these codes passed with 81/100

--- PercolationStats.java
+++ PercolationStats.java
@@ -1,88 +1,73 @@
-import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.StdOut;
 
 public class PercolationStats {
-    double[] fractionslogged;
-    int n;
-    int trials;
+    private double[] fractionslogged;
+    private int trials;
+    private static final double CONFIDENCE_95 = 1.96;
 
     // perform independent trials on an n-by-n grid
-    public PercolationStats(int n, int trials){
-        if (n<=0 ||trials<=0) {
-            throw new IllegalArgumentException ("n cannot be <=0. trials cannot be <=0");
-            
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException("n cannot be <= 0. trials cannot be <= 0");
         }
-        this.n = n;
         this.trials = trials;
         this.fractionslogged = new double[trials];
 
-        for (int t=0;t<trials;t++){
+        for (int t = 0; t < trials; t++) {
             Percolation grid = new Percolation(n);
             int counter = 0;
-            while(!grid.percolates()){
+            while (!grid.percolates()) { 
                 counter++;
-                int randomintX = StdRandom.uniformInt(1,n+1);
-                int randomintY = StdRandom.uniformInt(1,n+1);
-                System.out.println("Randomizedint X:"+randomintX+" Y:"+randomintY);
-                grid.open(randomintX,randomintY);
-                System.out.println("Opening time "+counter);
+                int randomintX = StdRandom.uniformInt(1, n+1);
+                int randomintY = StdRandom.uniformInt(1, n+1);
+                // System.out.println("Randomizedint X:"+randomintX+" Y:"+randomintY);
+                grid.open(randomintX, randomintY);
+                // System.out.println("Opening time "+counter);
             }
-            System.out.println("Percolation for test "+t+"done in "+counter+" time!");
+            // System.out.println("Percolation for test "+t+"done in "+counter+" time!");
              
-            this.fractionslogged[t] = (double) counter/(n*n);
+            this.fractionslogged[t] = (double) counter / (n * n);
         }
     }
 
     // sample mean of percolation threshold
-    public double mean(){
-        double totalfractions = 0.00;
-
-        for (double i : this.fractionslogged){
-            totalfractions+= i;
-        }
-        return totalfractions/trials;
+    public double mean() {
+        return StdStats.mean(fractionslogged);
     }
 
     // sample standard deviation of percolation threshold
-    public double stddev(){
-        double totalsum = 0.00;
-        double mean = this.mean();
-        for (double i : this.fractionslogged){
-            totalsum+= Math.pow((i-mean),2);
-        }
-        return Math.sqrt(totalsum/(this.trials-1));
+    public double stddev() {
+        return StdStats.stddev(this.fractionslogged);
     }
 
     // low endpoint of 95% confidence interval
-    public double confidenceLo(){
+    public double confidenceLo() {
         double mean = mean();
         double stdev = stddev();
-        return mean-((1.96*stdev)/Math.sqrt(this.trials));
+        return mean - ((CONFIDENCE_95*stdev) / Math.sqrt(this.trials));
     }
 
     // high endpoint of 95% confidence interval
-    public double confidenceHi(){
+    public double confidenceHi() {
         double mean = mean();
         double stdev = stddev();
-        return mean+((1.96*stdev)/Math.sqrt(this.trials));
+        return mean + ((CONFIDENCE_95*stdev) / Math.sqrt(this.trials));
     }
 
    // test client (see below)
-   public static void main(String[] args){
-    //WIP
-       System.out.println("Enter for n");
-       int n = StdIn.readInt();
-       System.out.println("Enter for trials");
-       int trials = StdIn.readInt(); 
-       System.out.println("Starting");
+   public static void main(String[] args) {
 
-       PercolationStats statsholder = new PercolationStats(n,trials);
-       System.out.println("Mean: "+ statsholder.mean());
-       System.out.println("Standard Deviation: "+ statsholder.stddev());
-       System.out.println("95% confidence interval: ["+ statsholder.confidenceLo()+", "+statsholder.confidenceHi()+"]");
+       int n = Integer.parseInt(args[0]);
+       int t = Integer.parseInt(args[1]);
+
+       PercolationStats statsholder = new PercolationStats(n, t);
+       String confidence = statsholder.confidenceLo() + ", " + statsholder.confidenceHi();
+       StdOut.println("mean                    = " + statsholder.mean());
+       StdOut.println("stddev                  = " + statsholder.stddev());
+       StdOut.println("95% confidence interval = " + confidence);
        
-        
        }
     
 }
